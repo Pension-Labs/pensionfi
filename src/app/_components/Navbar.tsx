45b79bacc7c77f2b6d355d79c940c3e9f7b683fd
jmarquez <hello.jmarquez@gmail.com>
2024-06-20 07:41:49 -0500

    maqueta navbar añadida

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
+	return (
+		<nav className="bg-gray-800">
+			<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+				<div className="relative flex items-center justify-between h-16">
+					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+						{/* Mobile menu button */}
+					</div>
+					<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+						<div className="flex-shrink-0">{/* Logo */}</div>
+						<div className="hidden sm:block sm:ml-6">
+							<div className="flex space-x-4">
+								{/* Navigation links */}
+								<a
+									href="#"
+									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+								>
+									Inicio
+								</a>
+								<a
+									href="#"
+									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+								>
+									Servicios
+								</a>
+								<a
+									href="#"
+									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+								>
+									Acerca de
+								</a>
+								<Link
+									href="/contact"
+									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+								>
+									Contacto
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { IdiomaProvider } from '../Context/IdiomaContext';
+import Navbar from './_components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
+				<Navbar />
 				<IdiomaProvider>{children}</IdiomaProvider>
 			</body>
 		</html>
